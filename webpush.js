327fff6eaa235e8b2aac673222bcdbb101e71043
gilbert-24task <52233936+gilbert-24task@users.noreply.github.com>
2022-03-31 11:16:51 +0800

    update keys

--- webpush.js
+++ webpush.js
@@ -2,9 +2,8 @@ const webpush = require('web-push');
 
 // VAPID keys should only be generated only once.
 const vapidKeys = {
-  publicKey:
-    'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
-  privateKey: 'CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8'
+  publicKey: 'BJ9PhAbQ3OjBuk9BFZeNRUPlv_JGfdpmAVkWmFFzJmQ5FLDVVNuzOTdCrINH05xp2ExMPkc5jfVK9Jk77KHKpXE',
+  privateKey: 'RBLAawlMUnjHlCCo9Oiuta1vOqq1FGxKZXxre1DZBiQ'
 };
 
 webpush.setVapidDetails(
